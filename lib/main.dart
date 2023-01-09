311ab8e2aab0ec0e205ed91aaac166a045ee6b02
minseo-and <msp05070478@gmail.com>
2023-01-09 18:11:10 +0900

    column, row 정의

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,38 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello flutter!'),
-          ),
-          body: Center(
-            child: Text('Hello World'),
+          backgroundColor: Colors.black,
+          body: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.
+                end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text('박민서님',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text('사료는 가져오셨죠?',
+                        style: TextStyle(
+                            color: Colors.white.
+                            withOpacity(0.8),
+                            fontSize: 18
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
         ),
     );
